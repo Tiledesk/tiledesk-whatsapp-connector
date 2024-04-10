5d36877482d4e7ab8b754135314ecfc572de7f6c
Glitch (tiledesk-whatsapp-connector-dev) <none>
2024-04-10 12:38:38 +0000

    ✈️👨‍❤️‍👨 Checkpoint ./index.js:56161507/4602

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 //var mongoose = require('mongoose');
 
 // const whatsapp = require('@tiledesk/tiledesk-whatsapp-connector');
-const whatsapp = require('./whatsappRoute');
+const whatsapp = require("./whatsappRoute");
 const whatsappRoute = whatsapp.router;
 
-app.use("/", whatsappRoute)
+app.use("/", whatsappRoute);
 
 const BASE_URL = process.env.BASE_URL;
 const API_URL = process.env.API_URL;
 const BASE_FILE_URL = process.env.BASE_FILE_URL;
 const GRAPH_URL = process.env.GRAPH_URL;
 const MONGODB_URL = process.env.MONGODB_URL;
-const LOG_MONGODB_URL = process.env.MONGODB_URL_PRE || process.env.MONGODB_URL
+const LOG_MONGODB_URL = process.env.MONGODB_URL_PRE || process.env.MONGODB_URL;
 const APPS_API_URL = process.env.APPS_API_URL;
 const REDIS_HOST = process.env.REDIS_HOST;
 const REDIS_PORT = process.env.REDIS_PORT;
@@ -36,17 +36,20 @@ whatsapp.startApp(
     REDIS_PASSWORD: REDIS_PASSWORD,
     AMQP_MANAGER_URL: AMQP_MANAGER_URL,
     JOB_TOPIC_EXCHANGE: JOB_TOPIC_EXCHANGE,
-    log: log
-  }, (err) => {
-
+    log: log,
+  },
+  (err) => {
     if (!err) {
-      console.log("Whatsapp route succesfully started.")
+      console.log("Whatsapp route succesfully started.");
       var port = process.env.PORT || 3000;
-      app.listen(port, function() {
+      app.listen(port, function () {
         console.log("Whatsapp connector listening on port: ", port);
-      })
+      });
     } else {
-      console.log("(Warning) Unable to start tiledesk-whatsapp-connector.", err);
+      console.log(
+        "(Warning) Unable to start tiledesk-whatsapp-connector.",
+        err
+      );
     }
   }
 );
@@ -55,7 +58,7 @@ whatsapp.startApp(
  * Alternative start passing ad external db client
  */
 // mongoose.connect(MONGODB_URL, { useNewUrlParser: true, autoIndex: true }, function(err) {
-//   if (err) { 
+//   if (err) {
 //     console.error('Failed to connect to MongoDB on ' + databaseUri + " ", err);
 //     process.exit(1);
 //   }
